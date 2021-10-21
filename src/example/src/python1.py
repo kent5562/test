10872a87ebf32221552c2314f33eb1dcb30c2668
kent5562-enhao <judy16661k@gmail.com>
2021-10-21 21:17:39 +0800

    lesson python2

---
+++ src/example/src/python1.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+import python1
+x = input("請輸入密碼:")
+x = len(aa)
+if x=aa:
+print("")
+
+else:
+print("密碼有誤")
---
+++ src/example/src/python2.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+from time import sleep
+
+n=1
+while n<11:
+    
+    print(n)
+    n+=1
+    sleep(1)
